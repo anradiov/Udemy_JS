d5d8c30e262805901298316186137ec43286f1cf
Andis <anradiov@gmail.com>
2021-01-09 23:27:28 +0200

    Added for [p1][v1.0.0]

--- js/p1script.js
+++ js/p1script.js
@@ -25,9 +25,11 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Один из последних просмотренных фильмов?'),
-      b = prompt('На сколько оцените его?');
-      
-personalMovieDB.movies[a] = b;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?'),
+          b = +prompt('На сколько оцените его?');
+    personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
