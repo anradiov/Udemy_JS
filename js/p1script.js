38cb4769e6df759b355524d368dd6ad1cdf1590a
Andis <anradiov@gmail.com>
2021-01-10 00:03:43 +0200

    fixed IF statment [p1][v1.0.0]

--- js/p1script.js
+++ js/p1script.js
@@ -56,7 +56,7 @@ while (i < 2) {
 
 if (personalMovieDB.count < 10) {
     console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
     console.log('Вы классический зритель');
 } else if (personalMovieDB.count >= 30) {
     console.log('Вы киноман');
