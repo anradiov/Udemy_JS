09db78baf17756b2c36a3b7f0e9d4cb924343d3b
Andis <anradiov@gmail.com>
2021-01-09 23:42:31 +0200

    If -> check for unvalid data[p1][v1.0.0]

--- js/p1script.js
+++ js/p1script.js
@@ -29,7 +29,14 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?',''),
           b = +prompt('На сколько оцените его?','');
-    personalMovieDB.movies[a] = b;
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
 }
 
 console.log(personalMovieDB);
