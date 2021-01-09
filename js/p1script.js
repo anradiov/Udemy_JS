417c939ec6f3239115dff1e9a6ba3ffeaad08b2e
Andis <anradiov@gmail.com>
2021-01-09 23:58:08 +0200

    Added while loop simmilar to for [p1][v1.0.0]

--- js/p1script.js
+++ js/p1script.js
@@ -26,13 +26,27 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?',''),
+//           b = +prompt('На сколько оцените его?','');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+while (i < 2) {
     const a = prompt('Один из последних просмотренных фильмов?',''),
           b = +prompt('На сколько оцените его?','');
     
     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
         console.log('done');
+        i++;
     } else {
         console.log('error');
         i--;
