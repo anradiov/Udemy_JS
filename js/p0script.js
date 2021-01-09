c603b88d7a8773107eff1165a99782c625eb588e
Andis <anradiov@gmail.com>
2021-01-09 22:56:53 +0200

    Clg to {}.movies [p0][v1.0.0]

--- js/p0script.js
+++ js/p0script.js
@@ -36,5 +36,8 @@ const a = prompt('Один из последних просмотренных ф
       b = prompt('На сколько оцените его?'),
       c = prompt('Один из последних просмотренных фильмов?'),
       d = prompt('На сколько оцените его?');
+
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB.movies);
