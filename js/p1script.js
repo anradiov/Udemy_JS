118180ddd9e588f51dfcde3d633090ea9d751b28
Andis <anradiov@gmail.com>
2021-01-10 00:08:59 +0200

    Added If NaN support [p1][v1.0.0]

--- js/p1script.js
+++ js/p1script.js
@@ -60,7 +60,7 @@ do {
     const a = prompt('Один из последних просмотренных фильмов?',''),
           b = +prompt('На сколько оцените его?','');
     
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && !isNaN(b)) {
         personalMovieDB.movies[a] = b;
         console.log('done');
         i++;
