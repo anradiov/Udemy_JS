23c6694e3252f22d917e9042182da8b37b1bdf1d
Andis <anradiov@gmail.com>
2021-01-09 22:57:31 +0200

    Script src changed to p0 [index.html][v1.0.0]

--- js/p0script.js
+++ js/p0script.js
@@ -40,4 +40,4 @@ const a = prompt('Один из последних просмотренных ф
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB.movies);
+console.log(personalMovieDB);
