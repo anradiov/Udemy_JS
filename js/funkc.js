a4d842da738432becbbd5a372a8bd898d22d107b
Andis <anradiov@gmail.com>
2021-01-11 15:19:41 +0200

    func calc()

--- js/funkc.js
+++ js/funkc.js
@@ -1,7 +1,16 @@
 'use strict';
 
-function showFirstMessage() {
-    console.log('Hello World');
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 20;
 }
 
-showFirstMessage();
+showFirstMessage('Hello World');
+console.log(num);
+
+function calc (a,b) {
+    return (a + b);
+}
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
