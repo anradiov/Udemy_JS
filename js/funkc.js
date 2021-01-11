05871abf4e1f6e011bb1191aacf727f7a0752938
Andis <anradiov@gmail.com>
2021-01-11 15:21:26 +0200

    func ret()

--- js/funkc.js
+++ js/funkc.js
@@ -14,3 +14,12 @@ function calc (a,b) {
 console.log(calc(4, 3));
 console.log(calc(5, 6));
 console.log(calc(10, 6));
+
+function ret (){
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
